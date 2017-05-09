b77b646c107472ebae1e2c8bc5dd7736aa4ee350
Igor Kandaurov <igorkandaurov@gmail.com>
2017-05-09 13:58:11 +0300

    Минимальная правка

--- pubmqtt.lua
+++ pubmqtt.lua
@@ -1,28 +1,27 @@
 M={}
 M.publ = function(answer, call)
-    answer.heap = ""..node.heap()
-    local sendMQ
-    local getd = coroutine.create(function()
-        for k, v in pairs(answer) do
-            sendMQ(k, v)
-            coroutine.yield()
-        end
-			-- 06.05.2017 clear table to send fot MQTT broker
-			_G.answer = {}
-            collectgarbage()
-			if call then
-                M.publ,sendMQ, getd, M  = nil, nil, nil, nil
-                package.loaded["pubmqtt"]=nil
-                call()                      
-            end
-    end)
-    sendMQ = function(k, v)
-        m:publish("from"..myClient.."/"..k,v,0,0, 
-            function(con) 
-                -- print("Send "..k.." = "..v)
-                coroutine.resume(getd)
-        end)
-    end
-    coroutine.resume(getd)
+	answer.heap = ""..node.heap()
+	local sendMQ
+	local getd = coroutine.create(function()
+		for k, v in pairs(answer) do
+			sendMQ(k, v)
+			coroutine.yield()
+		end
+		_G.answer = {}
+		collectgarbage()
+		if call then
+			M.publ,sendMQ, getd, M  = nil, nil, nil, nil
+			package.loaded["pubmqtt"]=nil
+			call()                      
+		end
+	end)
+	sendMQ = function(k, v)
+		m:publish("from"..myClient.."/"..k,v,0,0, 
+			function(con) 
+			-- print("Send "..k.." = "..v)
+			coroutine.resume(getd)
+		end)
+	end
+	coroutine.resume(getd)
 end
 return M
